5b7784207fe4fa089ff8dc74bd9332088e17b992
FS <yukuyoulei@hotmail.com>
2021-07-25 16:35:40 +0800

    增加Framework版本

---
+++ Fangchenmi.Framework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+	static void Main(string[] args)
+	{
+		Fangchenmi.OnDoFangchenmi(new
+			{
+				ai = "100000000000000001",
+				name = "某一一",
+				idNum = "110000190101010001",
+			}, Fangchenmi.EType.Check, "ctaM7Y");
+
+		//Fangchenmi.OnDoFangchenmi("100000000000000001", Fangchenmi.EType.Query, "ctaM7Y");
+
+		/*Fangchenmi.OnDoFangchenmi(new
+		{
+			collections = new object[]
+			{
+				new
+				{
+					no = 1,
+					pi = "1fffbkmd9ebtwi7u7f4oswm9li6twjydqs7qjv",
+					si = "111",
+					bt = 0,
+					ot = ApiDateTime.SecondsFrom19700101ms(),
+					ct = 0,
+					//di = "123333",
+				}
+			},
+		}, Fangchenmi.EType.Collect, "ctaM7Y");*/
+
+		Console.ReadKey();
+	}
+}
---
+++ packages/EasyEncryption.1.0.2/tools/init.ps1
@@ -0,0 +1,2 @@
+param($installPath, $toolsPath, $package)
+$DTE.ItemOperations.Navigate("https://github.com/polischuk/EasyEncryption/wiki/Usage-wiki")
